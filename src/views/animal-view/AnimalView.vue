9a144fac22c908eb0139b94ee734cec4ffc1a1a3
YakovTheDeveloper <90326974+YakovTheDeveloper@users.noreply.github.com>
2025-07-16 14:02:30 +0400

    fix

--- src/views/animal-view/AnimalView.vue
+++ src/views/animal-view/AnimalView.vue
@@ -9,21 +9,55 @@
         {{ data?.name }}
       </p>
       <div class="col" :style="{ gap: '20px' }">
-        <AnimalViewBlockOne :title="data?.blockOne[0].field1Name" :desc="data?.blockOne[0].field1Description" />
-        <AnimalViewBlockOne :title="data?.blockOne[0].field2Name" :desc="data?.blockOne[0].field2Description" />
-        <AnimalViewBlockOne :title="data?.blockOne[0].field3Name" :desc="data?.blockOne[0].field3Description" />
-        <AnimalViewBlockOne :title="data?.blockOne[0].field4Name" :desc="data?.blockOne[0].field4Description" />
-        <AnimalViewBlockOne :title="data?.blockOne[0].field5Name" :desc="data?.blockOne[0].field5Description" />
-        <AnimalViewBlockOne :title="data?.blockOne[0].field6Name" :desc="data?.blockOne[0].field6Description" />
-        <AnimalViewBlockOne :title="data?.blockOne[0].field7Name" :desc="data?.blockOne[0].field7Description" />
-        <AnimalViewBlockOne2 :title="data?.blockOne[0].field8Name" :desc="data?.blockOne[0].field8Description" />
-        <AnimalViewBlockOne2 :title="data?.blockOne[0].field9Name" :desc="data?.blockOne[0].field9Description" />
-        <AnimalViewBlockOne2 :title="data?.blockOne[0].field10Name" :desc="data?.blockOne[0].field10Description" />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field1Name"
+          :desc="data?.blockOne[0].field1Description"
+        />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field2Name"
+          :desc="data?.blockOne[0].field2Description"
+        />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field3Name"
+          :desc="data?.blockOne[0].field3Description"
+        />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field4Name"
+          :desc="data?.blockOne[0].field4Description"
+        />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field5Name"
+          :desc="data?.blockOne[0].field5Description"
+        />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field6Name"
+          :desc="data?.blockOne[0].field6Description"
+        />
+        <AnimalViewBlockOne
+          :title="data?.blockOne[0].field7Name"
+          :desc="data?.blockOne[0].field7Description"
+        />
+        <AnimalViewBlockOne2
+          :title="data?.blockOne[0].field8Name"
+          :desc="data?.blockOne[0].field8Description"
+        />
+        <AnimalViewBlockOne2
+          :title="data?.blockOne[0].field9Name"
+          :desc="data?.blockOne[0].field9Description"
+        />
+        <AnimalViewBlockOne2
+          :title="data?.blockOne[0].field10Name"
+          :desc="data?.blockOne[0].field10Description"
+        />
       </div>
     </div>
     <div class="center-1 block">
       <div class="img">
-        <img :src="getServerImageUrl(data?.blockTwoMedia?.image1)" :style="{ borderRadius: '24px' }" alt="" />
+        <img
+          :src="getServerImageUrl(data?.blockTwoMedia?.image1)"
+          :style="{ borderRadius: '24px' }"
+          alt=""
+        />
       </div>
       <div class="col" v-if="data?.blockTwo[0].field11Name">
         <p class="sub">{{ data?.blockTwo[0].field11Name }}</p>
@@ -48,40 +82,85 @@
         {{ data?.blockThree[0]?.field14Description }}
       </p>
       <div class="col" :style="{ gap: '30px', marginTop: '40px' }">
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field15Name" :desc="data?.blockThree[0]?.field15Description"
-          :img="data?.blockThreeMedia?.image15" />
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field16Name" :desc="data?.blockThree[0]?.field16Description"
-          :img="data?.blockThreeMedia?.image16" />
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field17Name" :desc="data?.blockThree[0]?.field17Description"
-          :img="data?.blockThreeMedia?.image17" />
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field18Name" :desc="data?.blockThree[0]?.field18Description"
-          :img="data?.blockThreeMedia?.image18" />
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field19Name" :desc="data?.blockThree[0]?.field19Description"
-          :img="data?.blockThreeMedia?.image19" />
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field20Name" :desc="data?.blockThree[0]?.field20Description"
-          :img="data?.blockThreeMedia?.image20" />
-        <AnimalViewBlockThree :title="data?.blockThree[0]?.field21Name" :desc="data?.blockThree[0]?.field21Description"
-          :img="data?.blockThreeMedia?.image21" />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field15Name"
+          :desc="data?.blockThree[0]?.field15Description"
+          :img="data?.blockThreeMedia?.image15"
+        />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field16Name"
+          :desc="data?.blockThree[0]?.field16Description"
+          :img="data?.blockThreeMedia?.image16"
+        />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field17Name"
+          :desc="data?.blockThree[0]?.field17Description"
+          :img="data?.blockThreeMedia?.image17"
+        />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field18Name"
+          :desc="data?.blockThree[0]?.field18Description"
+          :img="data?.blockThreeMedia?.image18"
+        />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field19Name"
+          :desc="data?.blockThree[0]?.field19Description"
+          :img="data?.blockThreeMedia?.image19"
+        />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field20Name"
+          :desc="data?.blockThree[0]?.field20Description"
+          :img="data?.blockThreeMedia?.image20"
+        />
+        <AnimalViewBlockThree
+          :title="data?.blockThree[0]?.field21Name"
+          :desc="data?.blockThree[0]?.field21Description"
+          :img="data?.blockThreeMedia?.image21"
+        />
       </div>
     </div>
     <div class="center-1-bottom block">
-      <img v-if="data?.blockTwoMedia.image2" :src="getServerImageUrl(data?.blockTwoMedia.image2)" class="radius" />
-      <img v-if="data?.blockTwoMedia.image3" :src="getServerImageUrl(data?.blockTwoMedia.image3)" class="radius" />
+      <img
+        v-if="data?.blockTwoMedia.image2"
+        :src="getServerImageUrl(data?.blockTwoMedia.image2)"
+        class="radius"
+      />
+      <img
+        v-if="data?.blockTwoMedia.image3"
+        :src="getServerImageUrl(data?.blockTwoMedia.image3)"
+        class="radius"
+      />
     </div>
     <div class="center-2-bottom block">
       <div :style="{ position: 'relative', height: '100%' }">
-        <video v-if="data?.blockThreeMedia.video" :src="getServerImageUrl(data?.blockThreeMedia?.video)" muted
-          playsinline class="no-timeline" @click="togglePlay" @play="isPlaying = true" @pause="isPlaying = false"
-          ref="videoRef">
-        </video>
-        <button v-if="data?.blockThreeMedia.video && !isPlaying" class="video-button" @click="togglePlay">
+        <video
+          v-if="data?.blockThreeMedia.video"
+          :src="getServerImageUrl(data?.blockThreeMedia?.video)"
+          muted
+          playsinline
+          class="no-timeline"
+          @click="togglePlay"
+          @play="isPlaying = true"
+          @pause="isPlaying = false"
+          ref="videoRef"
+        ></video>
+        <button
+          v-if="data?.blockThreeMedia.video && !isPlaying"
+          class="video-button"
+          @click="togglePlay"
+        >
           <PlayIcon width="40px" />
         </button>
       </div>
     </div>
     <div class="right-top block row" :style="{ gap: '40px' }">
-      <img v-if="data?.blockFourMedia?.image22" :src="getServerImageUrl(data?.blockFourMedia?.image22)" alt=""
-        :style="{ width: '270px' }" class="round" />
+      <img
+        v-if="data?.blockFourMedia?.image22"
+        :src="getServerImageUrl(data?.blockFourMedia?.image22)"
+        alt=""
+        :style="{ width: '270px' }"
+        class="round"
+      />
       <div class="col" :style="{ width: '580px', justifyContent: 'center' }">
         <p class="sub">{{ data?.blockFour[0]?.field22Name }}</p>
         <p>
@@ -90,30 +169,53 @@
       </div>
 
       <div class="col" :style="{ gap: '20px', justifyContent: 'space-evenly' }">
-        <div v-if="data?.blockFourMedia?.audio1" class="row" :style="{ gap: '20px', alignItems: 'center' }">
-          <button src="" alt="" :style="{ width: '100px' }" class="round" @click="toggleAudio('audio1')">
+        <div
+          v-if="data?.blockFourMedia?.audio1"
+          class="row"
+          :style="{ gap: '20px', alignItems: 'center' }"
+        >
+          <button
+            src=""
+            alt=""
+            :style="{ width: '100px' }"
+            class="round"
+            @click="toggleAudio('audio1')"
+          >
             <PlaySoundIcon />
           </button>
           <p class="sub_s">{{ data?.blockFour[0]?.audio1Name }}</p>
           <audio ref="audioRef" :src="getServerImageUrl(data?.blockFourMedia?.audio1)"></audio>
         </div>
-        <div v-if="data?.blockFourMedia?.audio2" class="row" :style="{ gap: '20px', alignItems: 'center' }">
-          <button src="" alt="" :style="{ width: '100px' }" class="round" @click="toggleAudio('audio2')">
+        <div
+          v-if="data?.blockFourMedia?.audio2"
+          class="row"
+          :style="{ gap: '20px', alignItems: 'center' }"
+        >
+          <button
+            src=""
+            alt=""
+            :style="{ width: '100px' }"
+            class="round"
+            @click="toggleAudio('audio2')"
+          >
             <PlaySoundIcon />
           </button>
           <p class="sub_s">{{ data?.blockFour[0]?.audio2Name }}</p>
           <audio ref="audioRef2" :src="getServerImageUrl(data?.blockFourMedia?.audio2)"></audio>
         </div>
       </div>
-
     </div>
     <div class="center-3 block">
       <p class="sub_s" :style="{ marginBottom: '20px' }">
         {{ data?.blockFive[0]?.field23Name }}
       </p>
       <div class="row" :style="{ gap: '20px', alignItems: 'center' }">
-        <img :src="getServerImageUrl(data?.blockFiveMedia?.image23)" alt=""
-          :style="{ minWidth: '150px', width: '150px', height: '150px' }" class="round" />
+        <img
+          :src="getServerImageUrl(data?.blockFiveMedia?.image23)"
+          alt=""
+          :style="{ minWidth: '150px', width: '150px', height: '150px' }"
+          class="round"
+        />
         <p>
           {{ data?.blockFive[0]?.field23Description }}
         </p>
@@ -174,30 +276,45 @@
             {{ data?.blockSix[0]?.field32Description }}
           </p>
         </div>
-        <img v-if="data?.blockSixMedia?.image32" :src="getServerImageUrl(data?.blockSixMedia?.image32)" alt=""
-          :style="{ width: '210px', minWidth: '210px', height: '210px' }" class="round" />
+        <img
+          v-if="data?.blockSixMedia?.image32"
+          :src="getServerImageUrl(data?.blockSixMedia?.image32)"
+          alt=""
+          :style="{ width: '210px', minWidth: '210px', height: '210px' }"
+          class="round"
+        />
       </div>
       <Space value="50" />
       <div class="row" :style="{ justifyContent: 'space-between' }">
         <div class="col" :style="{ width: '270px', gap: '10px' }">
           <p class="sub" :style="{ fontSize: '30px' }">{{ data?.blockSix[0]?.field33Name }}</p>
-          <img v-if="data?.blockSixMedia?.image33" :src="getServerImageUrl(data?.blockSixMedia?.image33)" alt="" :style="{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            minWidth: '210px',
-            borderRadius: '24px',
-          }" />
+          <img
+            v-if="data?.blockSixMedia?.image33"
+            :src="getServerImageUrl(data?.blockSixMedia?.image33)"
+            alt=""
+            :style="{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              minWidth: '210px',
+              borderRadius: '24px',
+            }"
+          />
         </div>
         <div class="col" :style="{ width: '580px', gap: '10px' }">
           <p class="sub" :style="{ fontSize: '30px' }">{{ data?.blockSix[0]?.field34Name }}</p>
-          <img v-if="data?.blockSixMedia?.image34" :src="getServerImageUrl(data?.blockSixMedia?.image34)" alt="" :style="{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            minWidth: '210px',
-            borderRadius: '24px',
-          }" />
+          <img
+            v-if="data?.blockSixMedia?.image34"
+            :src="getServerImageUrl(data?.blockSixMedia?.image34)"
+            alt=""
+            :style="{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              minWidth: '210px',
+              borderRadius: '24px',
+            }"
+          />
         </div>
       </div>
 
@@ -214,7 +331,7 @@
           <div v-html="data?.blockSix[0]?.field36Description"></div>
         </div>
         <Space value="30" />
-        <div :style="{ display: 'grid', gridTemplateColumns: '1fr 1fr' }">
+        <div :style="{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '30px' }">
           <div class="column" v-if="data?.blockSix[0]?.field37Name">
             <p class="sub" :style="{ fontSize: '30px', lineHeight: '130%' }">
               {{ data?.blockSix[0]?.field37Name }}
@@ -267,7 +384,7 @@ const toggleAudio = (which) => {
   }
 
   if (audio.paused) {
-    audio.play().catch(err => console.warn('Play error:', err))
+    audio.play().catch((err) => console.warn('Play error:', err))
   } else {
     audio.pause()
   }
@@ -277,29 +394,26 @@ watchEffect(() => {
   console.log(`output->data`, data.value)
 })
 
-
 const isPlaying = ref(false)
 const videoRef = ref(null)
 
-
 const togglePlay = () => {
-  const video = videoRef.value;
-  if (!video) return;
+  const video = videoRef.value
+  if (!video) return
 
   if (video.paused) {
-    video.play();
-    isPlaying.value = true;
+    video.play()
+    isPlaying.value = true
   } else {
-    video.pause();
-    isPlaying.value = false;
+    video.pause()
+    isPlaying.value = false
   }
-};
+}
 
 onMounted(() => {
-  const { id } = router.currentRoute.value.params;
-  store.fetchHabitat(+id);
-
-});
+  const { id } = router.currentRoute.value.params
+  store.fetchHabitat(+id)
+})
 </script>
 
 <style scoped lang="scss">
